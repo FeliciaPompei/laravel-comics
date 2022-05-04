fc819b4d3f342be9262228dcd1041cf955a1be95
FeliciaPompei <f.pompei@hotmail.com>
2022-05-04 16:22:39 +0200

    making the structure of the page

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,11 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home Page</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>
-        Hello World
-    </h1>
+    @include('partials.header')
+
+    <main>
+        I'm the main content
+    </main>
+
+    @include('partials.footer')
 </body>
 </html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,23 @@
+<header>
+    <nav>
+        <h1>I'm a header</h1>
+        <ul>
+            <li>
+                Item
+            </li>
+            <li>
+                Item
+            </li>
+            <li>
+                Item
+            </li>
+            <li>
+                Item
+            </li>
+            <li>
+                Item
+            </li>
+
+        </ul>
+    </nav>
+</header>
